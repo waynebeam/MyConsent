07287ea7cf00384563afb383867e5e27e19bc3a5
waynebeam <waynejcbeam@me.com>
2022-10-04 12:43:51 -0400

    url pattern in pbm urls.py

--- myconsent/PBMConsent/urls.py
+++ myconsent/PBMConsent/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = 'pbm'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('pbm', views.pbmquestions, name='pbmquestions'),
     path('pbm/consent', views.consent, name='pbmconsent')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
