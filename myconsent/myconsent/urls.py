81dcdd75fdebfd4842d41acda5f8ad5b3f57a913
waynebeam <waynejcbeam@me.com>
2022-10-04 12:13:47 -0400

    urls

--- myconsent/myconsent/urls.py
+++ myconsent/myconsent/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('PBMConsent.urls')),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
