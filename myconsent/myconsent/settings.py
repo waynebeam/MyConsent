9e1403987e3f3840d1f356bb48291ef27b2651cd
waynebeam <waynejcbeam@me.com>
2022-10-04 08:41:25 -0400

    statics2

--- myconsent/myconsent/settings.py
+++ myconsent/myconsent/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'PBMConsent', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
